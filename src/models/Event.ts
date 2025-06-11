db01326fa6d65cf2ab22239e5cd9f7f5ba4173cc
szervik <szerepi.viktor@gmail.com>
2025-06-11 16:44:44 +0200

    Event class added

---
+++ src/models/Event.ts
@@ -0,0 +1,25 @@
+import { IEvent } from "../interfaces/IEvent";
+import { IGuest } from "../interfaces/IGuest";
+
+
+export class Event<T extends IGuest> implements IEvent<T> {
+    guests: Map<string, T>;
+
+    constructor(
+        public id: number,
+        public name: string,
+        public date: Date,
+        public place: string,
+        public type: string
+    ) {
+        this.guests = new Map<string, T>();
+    }
+
+    addGuest(guest: T): void {
+        this.guests.set(guest.email, guest);
+    }
+
+    removeGuest(email: string): void {
+        this.guests.delete(email);
+    }
+}
